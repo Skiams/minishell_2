f943a372ee46589c867bda480528284274918efe
elsa2491 <elsa.touma@gmail.com>
2024-06-05 17:20:39 +0200

    work on tmp is here_doc_error

--- srcs/exec/here_doc_error.c
+++ srcs/exec/here_doc_error.c
@@ -6,7 +6,7 @@
 /*   By: eltouma <eltouma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 22:19:04 by eltouma           #+#    #+#             */
-/*   Updated: 2024/05/31 18:39:22 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/06/05 17:18:47 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,16 @@ void	ft_is_max_here_doc_nb_reached(t_data *data, t_cmds *cmds)
 	t_cmds	*tmp;
 
 	tmp = cmds;
-	while (cmds != NULL)
+	while (tmp != NULL)
 	{
-		cmds->here_doc_count = ft_count_here_doc(cmds);
-		if (cmds->here_doc_count > 16)
+		tmp->here_doc_count = ft_count_here_doc(tmp);
+		if (tmp->here_doc_count > 16)
 		{
 			ft_putstr_fd("minishell: ", 2);
 			ft_putstr_fd("maximun here-doc count exceeded\n", 2);
-			ft_exit_properly(data, cmds);
+			// a modifier opur eviter les leaks si > 16 n'est pas le premier hd inout de minishell
+			ft_exit_properly(data, tmp);
 		}
-		cmds = cmds->next;
+		tmp = tmp->next;
 	}
-	cmds = tmp;
 }
