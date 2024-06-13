258452007c9a6aa393bfd40beaf93c66ad3c65d9
elsa2491 <elsa.touma@gmail.com>
2024-06-13 16:59:55 +0200

    [13.06 fd open hd] Fix fd open with command not found

--- srcs/exec/free_functions.c
+++ srcs/exec/free_functions.c
@@ -6,7 +6,7 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 16:04:22 by eltouma           #+#    #+#             */
-/*   Updated: 2024/06/11 15:25:09 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/06/13 16:57:21 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,11 @@ void	*ft_free_tab(char **tab)
 
 void	ft_free(t_data *data, t_cmds *tmp, char *cmd, char *error)
 {
+	dprintf(2, "\t->%s\n\n", __func__);
 	t_cmds	*cmds;
 
 	cmds = tmp;
+	ft_close_hd_in_fork(data->cmd_list, NULL);
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd(": ", 2);
